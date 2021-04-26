0c48b9acc9a4dfeba8180fe6caa11ed60c349979
Nei Barros <nei.barros@amakhaparis.com.br>
2021-04-26 14:34:41 -0300

    Add theme and configure, adjustes config on storybook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,27 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Dashboard</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
+        <meta name="theme-color" content="#14131d" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A simple dashboard created with Next.js"
         />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,47 @@
+export default {
+  grid: {
+    container: '100%',
+    gutter: '3.2rem'
+  },
+  border: {
+    radius: '10rem'
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem'
+    }
+  },
+  colors: {
+    primary: '#14131d',
+    mainBg: '#f3f5fa',
+    white: '#FAFAFA',
+    red: '#dc4f3b',
+    lightGray: '#e0e5ee',
+    gray: '#d9d9d9'
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    alwaysOnTop: 40
+  }
+}
