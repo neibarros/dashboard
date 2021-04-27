9ed13e82c56ba0f8d1211ec4391009278554b557
Nei Barros <nei.barros@amakhaparis.com.br>
2021-04-26 21:46:33 -0300

    Add component base Dropdown

---
+++ src/components/Dropdown/styles.ts
@@ -0,0 +1,55 @@
+import styled, { css } from 'styled-components'
+
+export const Title = styled.div`
+  ${({ theme }) => css`
+    cursor: pointer;
+    color: ${theme.colors.primary};
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding-right: 2.4rem;
+  `}
+`
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    background-color: ${theme.colors.primary};
+    color: ${theme.colors.white};
+    /* margin-top: ${theme.spacings.small}; */
+    position: absolute;
+    right: 0;
+  `}
+`
+
+type WrapperProps = {
+  isOpen?: boolean
+}
+
+const wrapperModifiers = {
+  open: () => css`
+    opacity: 1;
+    transform: translateY(0);
+  `,
+  close: () => css`
+    opacity: 0;
+    pointer-events: none;
+    transform: translateY(-1rem);
+  `
+}
+
+export const Wrapper = styled.div<WrapperProps>`
+  ${({ theme, isOpen }) => css`
+    position: relative;
+    width: max-content;
+
+    ${Content} {
+      transition: transform ${theme.transition.default} ease-in,
+        opacity ${theme.transition.default};
+
+      ${isOpen && wrapperModifiers.open()};
+      ${!isOpen && wrapperModifiers.close()};
+    }
+  `}
+`
