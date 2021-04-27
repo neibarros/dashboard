eff2bd7ccdd93036d2a211862cd5b2530b6f1515
Nei Barros <nei.barros@amakhaparis.com.br>
2021-04-27 11:32:17 -0300

    Add component Avatar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Avatar = () => (
+  <S.Wrapper>
+    <S.Image src="/img/profile.jpg" alt="Bussinessman portrait picture" />
+  </S.Wrapper>
+)
+
+export default Avatar
---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  width: 4.8rem;
+  height: 4.8rem;
+  border-radius: 50%;
+  overflow: hidden;
+`
+
+export const Image = styled.img`
+  width: 4.8rem;
+  height: 4.8rem;
+`
